68177174ff9bb148abcd0775af7aa5005a0d227a
NTBNgoc <nguyenngoc.hust.97@gmail.com>
2019-01-05 15:07:42 +0700

    init project

---
+++ resources/js/components/MyComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="my-component">
+        <div>{{ message.type }}</div>
+        <div>{{ message.text }}</div>
+        <div><button @click="changeMessage">Click me</button></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: {
+                type: 'hello',
+                text: 'How are you?'
+            }
+        }
+    },
+    methods: {
+        changeMessage() {
+            this.message.type = 'hi'
+            this.message.text = 'I am fire thanks'
+        }
+    },
+    watch: {
+        message: {
+            handler: function () {
+                console.log('something changed')
+            },
+            deep: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .my-component {
+        color: red;
+    }
+</style>
